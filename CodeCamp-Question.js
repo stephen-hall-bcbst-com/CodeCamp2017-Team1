9ea9d524b14f3408ff4a9fead126b6f57384ff1a
joel roberson <joelroberson8646@gmail.com>
2017-07-19 12:32:47 -0400

    added questions

--- CodeCamp-Question.js
+++ CodeCamp-Question.js
@@ -13,20 +13,32 @@ module.exports = {
 var lc = question.toLowerCase();
 var rep = 'I don\'t know that, sorry';
 
-    if (lc.includes('how') && lc.includes('are') && lc.includes('you')); {
-    rep = 'I\'m doing as good as expected to be living in a computer'; 
-}
-    if (lc.includes('holiday') && lc.includes('january')); {
+        if (lc.includes('how') && lc.includes('are') && lc.includes('you')); {
+         rep = 'I\'m doing as good as expected to be living in a computer'; 
+    }
+        // January response 
+        if (lc.includes('holiday') && lc.includes('january')); {
         rep = 'January has lots of holidays most notably "New Years Day", "Martin Luther King JR\'s birthday", and "National Pie day." Whitch one do you want to know about. '; 
     }
-      
-    if (lc.includes('holiday') && lc.includes('february')) {
+        // February response 
+        if (lc.includes('holiday') && lc.includes('february')) {
         rep = 'oh I know about February. It has "Bubble Gum Day", "SuperBowl Sunday", and "National Freedom Day." Whitch one do you want ot know about. ';
     } 
-    if (lc.includes('holiday') && lc.includes('march')){
-        rep = 'I see you are interested in march, so here you go, "
+        // March response 
+        if (lc.includes('holiday') && lc.includes('march')) {
+        rep = 'I see you are interested in march, so here you go, "Peach Blossom Day", "Plant a Flower Day", and "St. Patricks Day." Whitch one do you want ot know about.'; 
     }  
-      
+        // April response 
+        if (lc.includes('holiday') && lc.includes('apirl')) {
+        rep = 'So, you want to know about Apirl. "Apirl Fools Day", "National Peanut Butter and Jelly Day", and "Zoo lovers Day". whitch one do you want to know about.';
+    }
+        if (lc.includes('holiday') && lc.includes('may')) { 
+            rep = 
+        }
+       
+       
+       
+       
         Slack.postMessageToChannel(channelName, rep );
 
         // *********************************************************************
