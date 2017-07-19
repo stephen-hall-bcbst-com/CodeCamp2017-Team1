b80561fc26b0fb15b6c872ca7cb742f5c774a862
Olivia Mills <oliviamills2604@gmail.com>
2017-07-19 09:10:20 -0400

    Help Progress

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -17,7 +17,7 @@ if (lc == "help"){
  Slack.postMessageToChannel(channelName, 'I can help you with Holdidays, and weather, as well as jokes.');   
 }
 if (lc == "help holidays"){
-Slack.postMessageToChannel(channelName, 'The Holidays are Halloween,');
+Slack.postMessageToChannel(channelName, 'The Holidays are Halloween, Easter, Valentines, Christmas, New Years, as well as Presidants Day.');
 }
         
 
