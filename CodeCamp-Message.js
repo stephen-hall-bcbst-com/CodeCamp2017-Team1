3ba5d527ce83a8d6304bd36437400b87ba39cdda
Olivia Mills <oliviamills2604@gmail.com>
2017-07-19 11:46:54 -0400

    Message Added

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -13,7 +13,7 @@ module.exports = {
         // *********************************************************************
 var lc = message.toLowerCase();
 var rep = 'Sorry, I did\'t catch that.'; 
-
+// January Holidays 
 if (lc == ('New Year\'s day')) {
     rep = ('To relax and have fun, it is also a time of joy.');
 }
@@ -23,8 +23,20 @@ if (lc == ('Martin Luther King jr\'s birthday')) {
  
  if (lc == ('National Pie day')) {
      rep == ('A day to cook and bake pies as well as to eat them.');
- }
- 
+}
+ // February Holidays
+ if (lc == ('Bubble Gum day')) {
+     rep == ('Kids bring 50 cents to school, to contribute to a good cause.');
+}
+
+if (lc == ('Superbowl Sunday')) {
+    rep == ('People throw parties as well as watch the game.');
+}
+
+if (lc == ('National Freedom day')) {
+    rep == ('Celebrates freedom from slavery, as well as America stands for a symbol of freedom.');
+}     
+        
         Slack.postMessageToChannel(channelName, rep);
 
         // *********************************************************************
