55c318abafc01227f4472181ce1c69a6c24e1bec
Olivia Mills <oliviamills2604@gmail.com>
2017-07-19 10:31:01 -0400

    Help progress & Message Progress

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -14,15 +14,9 @@ module.exports = {
 var lc = message.toLowerCase();
 
 if (lc == "help") {
-<<<<<<< HEAD
- Slack.postMessageToChannel(channelName, 'I can help you with Holdidays, and weather, as well as jokes.');   
-}
-if (lc == "help holidays") {
-=======
  Slack.postMessageToChannel(channelName, 'I can help you with Holdidays.');   
 }
 if (lc.includes("help") && lc.includes("holidays")) {
->>>>>>> 4ad0780ff7ac96893d9e25af9a54d73c74562465
 Slack.postMessageToChannel(channelName, 'The Holidays are Halloween, Easter, Valentines, Christmas, New Years, as well as Presidants Day.');
 };
 if (lc== "") { 
--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -12,10 +12,19 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 var lc = message.toLowerCase();
-var rep = 'Sorry, I did\'t catch that.'
+var rep = 'Sorry, I did\'t catch that.'; 
 
-
-
+if (lc == ('New Year\'s day')) {
+    rep = ('To relax and have fun, it is also a time of joy.');
+}
+if (lc == ('Martin Luther King jr\'s birthday')) {
+    rep == ('He grew to become a social activist, who believed strongly in equality.');
+}
+ 
+ if (lc == ('National Pie day')) {
+     rep == ('A day to cook and bake pies as well as to eat them.');
+ }
+ 
         Slack.postMessageToChannel(channelName, rep);
 
         // *********************************************************************
